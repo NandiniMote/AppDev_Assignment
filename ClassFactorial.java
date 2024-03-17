46dc570aba078b7904470eec360481ee34a6fc3c
Nandini <nandinipramod.mote2021@vitstudent.ac.in>
2024-03-18 01:03:16 +0530

    Modify ClassFactorial.java on nandini branch

--- ClassFactorial.java
+++ ClassFactorial.java
@@ -1,15 +1,11 @@
-class Factoral
-{
-	public static void main(String arg[])	
-	{
-             	int n=5,fact=1;
+class Factorial {
+    public static void main(String args[]) {
+        int n = 5, fact = 1;
 
-	        for(int i=1;i<=n;i++)
-	  	  {
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
 
-	    	   fact=fact*i;
- 	 	  }
- 
- 	        System.out.println("factoral="+fact);
-	}
-}          
+        System.out.println("Factorial = " + fact);
+    }
+}
